18fafbcf09d666d8fd0e8ac86f79b183581048ac
khalidonline <69767138+khalidonline@users.noreply.github.com>
2026-09-12 12:04:24 +0300

    fix: trust official Saudi Ministry recovery visual host

--- daily_news_runner.py
+++ daily_news_runner.py
@@ -744,6 +744,7 @@ OFFICIAL_VISUAL_HOSTS = {
     "ndmc.gov.sa", "www.ndmc.gov.sa", "sukuk.ndmc.gov.sa",
     "mof.gov.sa", "www.mof.gov.sa",
     "spa.gov.sa", "www.spa.gov.sa",
+    "pep.gov.sa", "www.pep.gov.sa",
     "apple.com", "www.apple.com",
 }
 CURATED_RECOVERY_VISUAL_HOSTS = {
